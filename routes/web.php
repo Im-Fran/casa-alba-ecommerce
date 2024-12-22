3aeadf69d6cd1b98cbb068f0a4e9af6801aedca2
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2024-12-22 16:34:26 -0300

    patch: fix livewire min route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,14 @@
 use App\Livewire\Home\HomePage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/livewire/livewire.min.js', fn() => response()->file(public_path('../vendor/livewire/livewire/dist/livewire.min.js')));
+Route::get('/livewire/livewire.min.js', function() {
+    $file = '../vendor/livewire/livewire/dist/livewire.min.js';
+    if(file_exists($file)) {
+        return response(file_get_contents($file))
+            ->header('Content-Type', 'application/javascript');
+    }
+
+    return response('Livewire not found', 404);
+});
 
 Route::get('/', HomePage::class)->name('home');
