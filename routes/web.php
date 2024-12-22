6f0ae4b397cd35f5598071beeeec84aa44ae4368
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2024-12-22 16:33:06 -0300

    patch: add livewire min route

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,6 @@
 use App\Livewire\Home\HomePage;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/livewire/livewire.min.js', fn() => response()->file(public_path('../vendor/livewire/livewire/dist/livewire.min.js')));
+
 Route::get('/', HomePage::class)->name('home');
