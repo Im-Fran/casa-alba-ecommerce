8600cba3064eac514ee6060ea4415d94ad16e622
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2024-12-22 17:17:41 -0300

    patch: remove livewire route

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,4 @@
 use App\Livewire\Home\HomePage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/livewire/livewire.min.js', function() {
-    $file = '../vendor/livewire/livewire/dist/livewire.min.js';
-    if(file_exists($file)) {
-        return response(file_get_contents($file))
-            ->header('Content-Type', 'application/javascript');
-    }
-
-    return response('Livewire not found', 404);
-});
-
 Route::get('/', HomePage::class)->name('home');
