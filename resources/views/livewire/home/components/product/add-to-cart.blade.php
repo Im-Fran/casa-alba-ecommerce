ec38d52ad063c924ab39441b534e6fd8ed3702fb
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2025-02-13 13:40:19 -0300

    patch: added no stock text

--- resources/views/livewire/home/components/product/add-to-cart.blade.php
+++ resources/views/livewire/home/components/product/add-to-cart.blade.php
@@ -5,9 +5,13 @@
             <span class="flex items-center justify-center text-xs md:text-base text-primary border border-primary rounded-md w-full {{ $size == 'sm' ? 'h-6 md:h-8' : 'h-8' }} font-bold">{{ $this->inCart() }} en Carrito</span>
             <x-button wire:click.stop="increase" class="btn btn-primary {{ $size == 'sm' ? 'btn-xs md:btn-sm' : 'btn-sm'  }} btn-circle text-neutral-50" icon="o-plus" spinner/>
         </div>
-    @elseif($this->stock == null)
+    @elseif($this->stock === null)
         <x-button class="btn btn-sm btn-disabled">
             Cargando Stock <span class="loading loading-spinner w-5 h-5"/>
+        </x-button>
+    @elseif($this->stock === 0)
+        <x-button class="btn btn-sm btn-disabled" icon-right="o-archive-box-x-mark">
+            Sin Stock
         </x-button>
     @else
         <x-button wire:click.stop="increase" class="btn btn-sm {{ $this->stock > 0 ? 'btn-primary' : 'btn-disabled' }}" icon-right="o-shopping-cart" spinner>
