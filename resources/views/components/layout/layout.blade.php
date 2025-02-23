5c8da102631c14dff844eb53c28ad1779a42f31c
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2025-02-23 16:17:31 -0300

    patch: improve favicon

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -7,7 +7,12 @@
     <title>{{ isset($title) ? "$title - Casa Alba" : 'Casa Alba' }}</title>
 
     <!-- Favicon -->
-    <link rel="icon" href="{{ asset('images/casaalba.webp') }}" type="image/webp">
+    <link rel="icon" type="image/png" href="{{ asset('/favicon-96x96.png') }}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset('/favicon.svg') }}" />
+    <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png') }}" />
+    <meta name="apple-mobile-web-app-title" content="Casa Alba" />
+    <link rel="manifest" href="{{ asset('/site.webmanifest') }}" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
